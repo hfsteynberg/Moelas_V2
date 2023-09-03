9febc73ff7f402c7be85e1e4c91752e567a21a4b
Frikkie3006 <21100292@virtualwindow.co.za>
2023-09-03 18:04:55 +0200

    Stuck Code - a dummy code will be created to show that the brief is understood with the requirements all being met, unfortunately there is no more time to get unstuck from the issue I am currently having.

--- app/src/main/java/com/example/moelasv2/TaxSkillsQuestionsActivity.kt
+++ app/src/main/java/com/example/moelasv2/TaxSkillsQuestionsActivity.kt
@@ -1,10 +1,15 @@
 package com.example.moelasv2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.RadioButton
+import android.widget.Toast
 import com.example.moelasv2.databinding.ActivityCategoriesBinding
 import com.example.moelasv2.databinding.ActivityTaxSkillsQuestionsBinding
+import models.constantsSavingSkills
+import models.constantsTaxSkills
 import models.constantsTaxSkills.allQuestionsTax
 import models.constantsTaxSkills.getAllQuestions
 
@@ -17,19 +22,78 @@ class TaxSkillsQuestionsActivity : AppCompatActivity() {
         val view=binding.root
         setContentView(view)
 
-        getAllQuestions()
-        val listOfQuestions = allQuestionsTax
+        var userScore = 0
 
-        Log.d("AAA First questions :", listOfQuestions[0].id.toString())
+        //val taxQuestionNumber = 1
+        //previouse error section - keeping it seprate to diagnose - START
+        val taxQuestionNumber = intent.extras?.getInt("questionNumberTax")
+        //previouse error section - keeping it seprate to diagnose - END
+
+
+
+        //added after error not loading - START
+        if(taxQuestionNumber == 1) {
+            constantsTaxSkills.getAllQuestions()
+        }
+        //added after error not loading - END
+
+
+        val listOfQuestions = constantsTaxSkills.allQuestionsTax
+
+
+        //redefining list of questions
+        //previouse error section - keeping it seprate to diagnose - START (Below is commented out because of the above error section
+        //val taxQuestionNumber: Int = 1
+        //previouse error section - keeping it seprate to diagnose - END
+
+
+
+        val taxCurrentQuestion = listOfQuestions[taxQuestionNumber!!-1]
 
         //Start of the actual backend of the transitions between questions and their validations
-        binding.taxQuestionTitle.text = "Tax Skills - Question ${listOfQuestions[0].id}"
-        binding.taxQuestionText.text = "" + listOfQuestions[0].questionTextTax
-        binding.rgTaxOptionOne.text = "" + listOfQuestions[0].optionOneTax
-        binding.rgTaxOptionTwo.text = listOfQuestions[0].optionTwoTax
-        binding.rgTaxOptionThree.text = listOfQuestions[0].optionThreeTax
-        binding.pbProgressTax.progress = listOfQuestions[0].id
-        binding.pbProgressTextTax.text = "${listOfQuestions[0].id}/${listOfQuestions.count()}"
+        binding.taxQuestionTitle.text = "Tax Skills - Question ${taxCurrentQuestion.id}"
+        binding.taxQuestionText.text = "" + taxCurrentQuestion.questionTextTax
+        binding.rgTaxOptionOne.text = "" + taxCurrentQuestion.optionOneTax
+        binding.rgTaxOptionTwo.text = taxCurrentQuestion.optionTwoTax
+        binding.rgTaxOptionThree.text = taxCurrentQuestion.optionThreeTax
+        binding.pbProgressTax.progress = taxCurrentQuestion.id
+        binding.pbProgressTextTax.text = "${taxQuestionNumber}/${listOfQuestions.count()}"
+        //tax question number is also added after error
 
+        binding.nextQuestionTax.setOnClickListener{
+            var selectedAnswerTax: Int = binding.rgTaxOptions.checkedRadioButtonId  //answer selected
+            Log.d("AAA Selected ID:", selectedAnswerTax.toString())
+
+            if(selectedAnswerTax != -1){
+                var selectedAnswerValueTax = findViewById<RadioButton>(selectedAnswerTax)
+                Log.d("AAA selected answer", selectedAnswerValueTax.text.toString())
+
+                if(selectedAnswerValueTax.text == taxCurrentQuestion.correctAnswerTax){
+                    userScore++
+                    Log.d("AAA user score is what:", userScore.toString())
+                }
+
+
+
+                //previouse error section - keeping it seprate to diagnose - START
+                val intent = Intent(this,TaxSkillsQuestionsActivity::class.java)
+                if (taxQuestionNumber != null) {
+                    intent.putExtra("questionNumberTax", taxQuestionNumber + 1)
+                }
+                //still fine up until here - if statement might change that
+                startActivity(intent)
+                //previouse error section - keeping it seprate to diagnose - END
+
+
+
+                //added after error not loading - END
+                finish()
+                //added after error not loading - END
+
+
+            } else {
+                Toast.makeText(this, "Please add your answer", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
